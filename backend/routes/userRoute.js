6c9d66b95e3f9e27db93368d9a94e256638d9153
Nitin-kumar-yadav <nitinkumar94555206@gmail.com>
2025-12-31 14:05:01 +0530

    feat: Add user authentication endpoints for signup, signin, and logout.

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { userSignin, userSignup } from "../controllers/userControll.js";
+import { logout, userSignin, userSignup } from "../controllers/userControll.js";
+import protectRoute from "../middleware/protectRoute.js"
 
 const router = express.Router();
 
 router.post('/signup', userSignup)
 router.post('/signin', userSignin)
+router.get('/logout', logout)
+router.get('/check', protectRoute, (req, res) => {
+    return res.status(200).json(req.user)
+})
 
 
 export default router;
