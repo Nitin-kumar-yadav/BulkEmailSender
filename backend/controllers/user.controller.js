d9eeae3a63c9200ea1fcb5c5a5f9ac8514e82300
Nitin-kumar-yadav <nitinkumar94555206@gmail.com>
2026-01-01 21:44:47 +0530

    feat: add user authentication controllers including signup, signin, logout, and OTP verification.

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,158 @@
+import { emailConnection } from "../lib/emailConnection.js";
+import { generateToken } from "../lib/utils.js";
+import UserModel from "../model/userModel.js";
+import bcrypt from "bcryptjs";
+
+
+export const userSignup = async (req, res) => {
+    const { Name, Email, Password } = req.body;
+
+    if (!Name || !Email || !Password) {
+        return res.status(400).json({
+            message: "All fields are required"
+        })
+    }
+    else if (Password.length < 6) {
+        return res.status(400).json({
+            message: "Password must be at least 6 characters long"
+        })
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(Email)) {
+        return res.status(400).json({
+            message: "Invalid email format"
+        })
+    }
+    try {
+        const user = await UserModel.findOne({ Email })
+        if (user) {
+            return res.status(400).json({
+                message: "User already exists"
+            })
+        }
+
+        const otp = Math.floor(100000 + Math.random() * 900000);
+        const otpExpiry = Date.now() + 60 * 60 * 1000;
+
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(Password, salt);
+        const newUser = await UserModel.create({
+            Name,
+            Email,
+            Password: hashedPassword,
+            Otp: otp,
+            OtpExpiry: otpExpiry
+        })
+
+        const emailSend = await emailConnection(otp, Email)
+
+        if (emailSend !== true) {
+            return res.status(400).json({
+                message: "Invaild email"
+            })
+        }
+        else if (newUser) {
+            return res.status(200).json({
+                message: "OTP sent successfully",
+            })
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        })
+    }
+}
+
+export const userSignin = async (req, res) => {
+    const { Email, Password } = req.body;
+    if (!Email || !Password) {
+        return res.status(400).json({
+            message: "All fields are required"
+        })
+    }
+    try {
+        const user = await UserModel.findOne({ Email })
+        if (!user) {
+            return res.status(400).json({
+                message: "User not found"
+            })
+        }
+        const isPasswordMatched = await bcrypt.compare(Password, user.Password);
+        if (!isPasswordMatched) {
+            return res.status(400).json({
+                message: "Password is incorrect"
+            })
+        }
+        generateToken(user._id, res);
+        return res.status(200).json({
+            _id: user._id,
+            Name: user.Name,
+            Email: user.Email,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Login function failed",
+            error: error.message
+        })
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({
+            message: "User logged out successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Logout function failed",
+            error: error.message
+        })
+    }
+}
+
+export const otpVerify = async (req, res) => {
+    const { otp } = req.body;
+    if (!otp) {
+        return res.status(401).json({
+            message: "Please enter OTP"
+        })
+    }
+    try {
+        const userOtp = await UserModel.findOne({ otp })
+        if (!userOtp) {
+            return res.status(401).json({
+                message: "User not found"
+            })
+        }
+        if (userOtp.Otp !== otp) {
+            return res.status(401).json({
+                message: "Invalid OTP"
+            })
+        }
+        if (userOtp.isVerified) {
+            return res.status(401).json({
+                message: "User already verified"
+            })
+        }
+        userOtp.isVerified = true;
+        userOtp.Otp = null;
+        userOtp.OtpExpiry = null;
+        await userOtp.save();
+        generateToken(userOtp._id, res);
+        return res.status(200).json({
+            message: "OTP verified successfully",
+            _id: userOtp._id,
+            Name: userOtp.Name,
+            Email: userOtp.Email,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "OTP verification failed",
+            error: error.message
+        })
+    }
+}
