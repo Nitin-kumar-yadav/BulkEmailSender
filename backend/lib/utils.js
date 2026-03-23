69c11a3a34f3ddab0bd6bfb3318b4e6ee3dc10f8
Nitin-kumar-yadav <nitinkumar94555206@gmail.com>
2026-03-23 19:23:32 +0530

    update utils.js file

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: NODE_ENV === "production",
-        sameSite: NODE_ENV === "production" ? "strict" : "lax",
+        sameSite: NODE_ENV === "production" ? "none" : "lax",
         maxAge: 24 * 60 * 60 * 1000
     })
     return token
